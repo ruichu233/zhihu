0951df6de113614a6f654061d78df875b9076d9d
zhang ruichu <zhangruichu@cyjiaomu.com>
2024-11-01 18:56:25 +0800

    code

--- app/like/internal/logic/getusertotallikeslogic.go
+++ app/like/internal/logic/getusertotallikeslogic.go
@@ -2,6 +2,10 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"zhihu/app/like/model"
 
 	"zhihu/app/like/internal/svc"
 	"zhihu/app/like/pb/like"
@@ -25,6 +29,29 @@ func NewGetUserTotalLikesLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 
 // 查询用户收到的总点赞数
 func (l *GetUserTotalLikesLogic) GetUserTotalLikes(in *like.GetUserTotalLikesRequest) (*like.GetUserTotalLikesResponse, error) {
+	// 1、 在缓存中查询点赞总数
+	count, err := l.svcCtx.RDB.Get(l.ctx, GetUserTotalLikesKey(in.UserId)).Int64()
+	if err == nil {
+		return &like.GetUserTotalLikesResponse{
+			TotalLikes: count,
+		}, nil
+	}
 
-	return &like.GetUserTotalLikesResponse{}, nil
+	if !errors.Is(err, redis.Nil) {
+		return nil, err
+	}
+	// 2、缓存中没有查询数据，查询数据库
+	if err := l.svcCtx.DB.Model(&model.LikeRecord{}).Where("user_id = ?", in.UserId).Count(&count).Error; err != nil {
+		return nil, errors.New("查询点赞总数失败")
+	}
+	// 3、更新缓存
+	l.svcCtx.RDB.Set(l.ctx, GetUserTotalLikesKey(in.UserId), count, 0)
+
+	return &like.GetUserTotalLikesResponse{
+		TotalLikes: count,
+	}, nil
+}
+
+func GetUserTotalLikesKey(userId int64) string {
+	return fmt.Sprintf("USER_TOTAL_LIKES_%d", userId)
 }
